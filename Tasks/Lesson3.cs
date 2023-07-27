9d416ba58eb55c8bcfc82440b9ef1fd250389539
doganes1507 <d.oganes1507@gmail.com>
2023-07-27 18:40:17 +0500

    Добавлены задачи по третьему уроку

---
+++ Tasks/Lesson3.cs
@@ -0,0 +1,45 @@
+﻿namespace Tasks;
+
+public static class Lesson3
+{
+    public static bool IsEven(int n)
+    {
+        // TODO: Реализовать функцию, принимающую целое число 'n' и возвращающую true, если число четное, иначе false
+
+        throw new NotImplementedException();
+    }
+    
+    public static void Calculator()
+    {
+        // TODO: Написать стандартный калькулятор для консоли: ввод двух переменных и символа операции между
+        // TODO: этими числами, затем вывод результата в консоль. Реализовать проверку на деление на ноль
+
+        throw new NotImplementedException();
+    }
+
+    public static int SpecialNumberCount(int from, int to)
+    {
+        // TODO: Реализовать функцию, которая считает количество чисел между числами 'from' и 'to', которые
+        // TODO: делятся на 3 или на 7
+        // "and" в Python == "&" в C#
+        // "or" в Python == "||" в C#
+
+        throw new NotImplementedException();
+    }
+
+    public static int SimpleDividerCount(int n)
+    {
+        // TODO: Реализовать функцию, подсчитывающую количество делителей числа 'n', включая 1 и само число
+        // TODO: 2 <= n <= 100000
+
+        throw new NotImplementedException();
+    }
+    
+    public static string ToBinary(int n)
+    {
+        // TODO: Реализовать функцию, принимающую целое число 'n' и преобразующую его
+        // TODO: в двоичный формат. Результат возвращается в виде string
+
+        throw new NotImplementedException();
+    }
+}
