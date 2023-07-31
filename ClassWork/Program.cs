1ae24948c0e50d80db5891ad9831cdf9d8dea860
Nikita <nikitauramne@gmail.com>
2023-07-31 18:44:00 +0300

    Выполнены задачи за 4 урок

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -4,6 +4,57 @@ public static class Program
 {
     public static void Main()
     {
+        // PrintList
+        Console.WriteLine("PrintList");
+        Tasks.Lesson4.PrintList(new List<int>{1, 3, 4});
+        Tasks.Lesson4.PrintList(new List<string>{"Hello world!", "Hi!", "Good bye!"});
+        Tasks.Lesson4.PrintList(new List<double>{1.5, 3.2, 4.6});
         
+        // CalculateAverage
+        Console.WriteLine();
+        Console.WriteLine("CalculateAverage");
+        Tasks.Lesson4.CalculateAverage(new List<double> { 1, 1.3, 3, 4 });
+        Tasks.Lesson4.CalculateAverage(new List<double>());
+        Tasks.Lesson4.CalculateAverage(new List<double> { 98, 2.3, 8, 16 });
+        
+        // CalculateSumAndProduct
+        Console.WriteLine();
+        Console.WriteLine("CalculateSumAndProduct");
+        Tasks.Lesson4.CalculateSumAndProduct(new List<double> { 1, 1.3, 3, 4 });
+        Tasks.Lesson4.CalculateSumAndProduct(new List<double>());
+        Tasks.Lesson4.CalculateSumAndProduct(new List<double> { 98, 2.3, 8, 16 });
+        
+        // CountNumbersInRange
+        Console.WriteLine();
+        Console.WriteLine("CountNumbersInRange");
+        Tasks.Lesson4.CountNumbersInRange(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1, 6);
+        Tasks.Lesson4.CountNumbersInRange(new List<int> { 19, 32, 67, 14, 51, 83, 97, 12, 100, 49 }, 35, 87);
+        Tasks.Lesson4.CountNumbersInRange(new List<int> { 1, 3, 13, 4, 43, 21, 9, 18, 19, 10 }, 3, 20);
+
+        // MultiplyList
+        Console.WriteLine();
+        Console.WriteLine("MultiplyList");
+        Tasks.Lesson4.MultiplyList(new List<int> { 1, 2, 3 }, 2);
+        Tasks.Lesson4.MultiplyList(new List<string> { "Hello", "Hi", "Good bye" }, 4);
+        Tasks.Lesson4.MultiplyList(new List<double> { 5.1, 21.2, 16.6 }, 6); 
+
+        // StopWord
+        Console.WriteLine();
+        Console.WriteLine("StopWord");
+        Tasks.Lesson4.StopWord(13);
+    
+        // OnlyEvens
+        Console.WriteLine();
+        Console.WriteLine("OnlyEvens");
+        Tasks.Lesson4.OnlyEvens(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        Tasks.Lesson4.OnlyEvens(new List<int> { 19, 32, 67, 14, 51, 83, 97, 12, 100, 49 });
+        Tasks.Lesson4.OnlyEvens(new List<int> { 1, 3, 13, 4, 43, 21, 9, 18, 19, 10 });
+        
+        // FilterByKeyword
+        Console.WriteLine();
+        Console.WriteLine("FilterByKeyword");
+        Tasks.Lesson4.FilterByKeyword(new List<string> { "hello", "world", "friend" }, "hello world, my friend!");
+        Tasks.Lesson4.FilterByKeyword(new List<string> { "hello", "hi", "good bye" }, "hello and good bye");
+        Tasks.Lesson4.FilterByKeyword(new List<string> { "like", "cucumber", "friend", "!" }, "i like cucumber!");
     }
 }
--- Tasks/Lesson4.cs
+++ Tasks/Lesson4.cs
@@ -4,101 +4,197 @@ namespace Tasks;
 
 public static class Lesson4
 {
-    // TODO: Все TODO перед коммитом необходимо удалить
-    // Комментарии желательно тоже, если только ты не пишешь комментарии по самому коду
     public static void PrintList<T>(List<T> list)
     {
-        // TODO: Каждый раз использовать циклы для вывода спсика - неудобно! Реализуй функцию, которая будет выводить
-        // TODO: в консоль принимаемый список. Примеры:
-        // { 1, 2, 3 }
-        // { Moscow, Paris, Washington } если значения стрковые
-        
-        // Тип данных T, указанный в типе списка - магический знак, обобщающий все типы (то есть дает возможность
-        // принимать любой тип (если только специально не ограничить T, например, только числовыми типами))
-        
-        // В новых версиях C# добавили возможность брать часть строки следующим образом:
-        // Console.WriteLine("Hello"[1..3]);
-        
-        // Также добавили возможность брать элементы индексом с конца
-        // Console.WriteLine("Hello"[^1]);
-        // Console.WriteLine("Hello"[^2]);
-        
-        throw new NotImplementedException();
+        var result = "{ ";
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            result += list[i];
+            if (i < list.Count - 1)
+            {
+                result += ",";
+                result += " ";
+            }
+        }
+
+        result += " }";
+
+        Console.WriteLine(result);
     }
 
-    public static double CalculateAverage<T>(List<T> list) where T: INumber<T>
+    public static double CalculateAverage<T>(List<T> list) where T : INumber<T>
     {
-        // TODO: На вход подается список с числовыми значениями (как дробными, так и целыми)
-        // TODO: Необходимо посчитать среднее арифметическое значений этого списка и вернуть его
-        // TODO: Если список пустой, вернуть 0
-        
-        throw new NotImplementedException();
+        if (list.Count != 0)
+        {
+            double sum = 0;
+
+            foreach (var t in list)
+            {
+                sum += Convert.ToDouble(t);
+            }
+
+            Console.WriteLine(sum / list.Count);
+            return sum / list.Count;
+        }
+
+        else
+        {
+            Console.WriteLine(0);
+            return 0;
+        }
     }
-    
+
     public static List<double> CalculateSumAndProduct<T>(List<T> list) where T : INumber<T>
     {
-        // TODO: На вход подается список с числовыми значениями (как дробными, так и целыми)
-        // TODO: Необходимо посчитать сумму и произведение всех чисел, а затем вернуть их в виде списка из двух чисел
-        
-        throw new NotImplementedException();
+        var result = new List<double>();
+
+        if (list.Count != 0)
+        {
+            double sum = 0;
+            double product = 1;
+
+            foreach (var t in list)
+            {
+                sum += Convert.ToDouble(t);
+            }
+
+            foreach (var t in list)
+            {
+                product *= Convert.ToDouble(t);
+            }
+
+            result.Add(sum);
+            result.Add(product);
+        }
+
+        PrintList(result);
+        return result;
     }
 
     public static int CountNumbersInRange(List<int> list, int min, int max)
     {
-        // TODO: На вход подается список целых чисел, а также два целых числа min и max
-        // TODO: Необходимо посчитать количество чисел в списке, удовлетворяющих условию: min <= list[i] <= max
-        
-        throw new NotImplementedException();
+        var count = 0;
+
+        foreach (var i in list)
+        {
+            if (min <= i & i <= max)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+        return count;
     }
-    
+
     public static List<T> MultiplyList<T>(List<T> list, int multiplier)
     {
-        // TODO: Еще нам очень не хватает умножения списка на число. Гарантируется, что 'multiplier' не может быть отрицательным
-
-        throw new NotImplementedException();
+        var newList = new List<T>();
+        for (int i = 0; i < multiplier; i++)
+        {
+            newList.AddRange(list);
+        }
+        
+        PrintList(newList);
+        return newList;
     }
-    
-    public static void StopWord(string stopWord)
-    {
-        // TODO: Пользователь вводит целые числа в консоль, каждое число на новой строке. Необходимо продолжать
-        // TODO: считывать числа, пока ввод пользователя не совпадет со строкой stopWord, которая подается на вход метода,
-        // TODO: после чего нужно из введенных значений вывести в консоль минимальное, максимальное,
-        // TODO: а также количество введенных чисел
 
-        throw new NotImplementedException();
+    public static int StopWord(int stopWord)
+    {
+        var count = 1;
+        var list = new List<int>();
+
+        while (true)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (stopWord == input)
+            {
+                list.Add(input);
+                break;
+            }
+
+            else
+            {
+                list.Add(input);
+                count++;
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(list.Min());
+        Console.WriteLine(list.Max());
+        Console.WriteLine(count);
+        
+        return count;
     }
 
     public static List<int> OnlyEvens(List<int> list)
     {
-        // TODO: На вход приходит список целых чисел - необходимо вернуть список, состоящий только из четных элементов изначального списка
+        var newList = new List<int>();
         
-        throw new NotImplementedException();
+        foreach (var element in list)
+        {
+            if (element % 2 == 0)
+            {
+                newList.Add(element);
+            }  
+        }
+
+        PrintList(newList);
+        return newList;
     }
 
     public static List<string> FilterByKeyword(List<string> list, string keyWord)
     {
-        // TODO: На вход приходит список строк, а также "ключевая строка". Необходимо вернуть список, в котором будут
-        // TODO: содержаться только те строки, которые содержат в себе ключевую строку.
-        // вместо оператора in из Python, в C# для строк (как и для списков) есть метод .Contains()
+        var newList = new List<string>();
 
-        throw new NotImplementedException();
+        foreach (var element in list)
+        {
+            if (keyWord.Contains(element))
+            {
+                newList.Add(element);
+            }
+        }
+
+        PrintList(newList);
+        return newList;
     }
 
-    public static void FilterTrash<T>(List<T> list)
+    public static List<double> FilterTrash<T>(List<T> list)
     {
-        // TODO: На вход подаается список значений самых разных типов. Необходимо вернуть список, состояющий
-        // TODO: только из элементов типа int и double. Для сравнения типов переменных в C# есть оператор is
-        // if (element is int)
-        
-        throw new NotImplementedException();
+        var newList = new List<double>();
+        foreach (var element in list)
+        {
+            if (element is double || element is int)
+            {
+                newList.Add(Convert.ToDouble(element));
+            }
+        }
+
+        PrintList(newList);
+        return newList;
     }
 
     public static List<int> SpecialUniteLists(List<int> list1, List<int> list2)
     {
-        // TODO: На вход подаются два списка целых чисел. Необходимо вернуть такой список, в котором содержатся
-        // TODO: все элементы первого списка, а также те элементы второго списка, которые не встречаются в первом
-        // Для списков {1, 2, 3, 3} и {1, 1, 4} результатом будет {1, 2, 3, 3, 4}
-        
-        throw new NotImplementedException();
+        var newList = list1;
+
+        foreach (var element in list2)
+        {
+            if (list1.Contains(element))
+            {
+                continue;
+            }
+            
+            else
+            {
+                newList.Add(element);
+            }
+        }
+
+        PrintList(newList);
+        return newList;
     }
 }
