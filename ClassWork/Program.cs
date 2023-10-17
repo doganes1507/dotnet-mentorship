5248c0cc187c5394de84706aa2152fc3a0466679
Nikita <nikitauramne@gmail.com>
2023-10-17 19:45:04 +0300

    Выполнены задачи за 6 урок

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -1,35 +1,45 @@
 ﻿namespace ClassWork;
-using static Tasks.Tasks5;
+
+using static Tasks.Tasks6;
 
 public static class Program
 {
     public static void Main()
     {
-        // IsPalindromes
-        Console.WriteLine("IsPalindromes");
-        Console.WriteLine(IsPalindromes("Казак"));
-        Console.WriteLine(IsPalindromes("ПотОп"));
-        Console.WriteLine(IsPalindromes("БреВнО"));
-
-        // IsPrime
-        Console.WriteLine();
-        Console.WriteLine("IsPrime");
-        Console.WriteLine(IsPrime(13));
-        Console.WriteLine(IsPrime(16));
-        Console.WriteLine(IsPrime(9));
-
-        // NumberOfVowels
-        Console.WriteLine();
-        Console.WriteLine("NumberOfVowels");
-        Console.WriteLine(NumberOfVowels("hEllo"));
-        Console.WriteLine(NumberOfVowels("GOOd"));
-        Console.WriteLine(NumberOfVowels("bYe"));
+        // PrintDictionary
+        Console.WriteLine("PrintDictionary");
+        PrintDictionary(new Dictionary<int, string>
+            {
+                { 1, "hello" },
+                { 2, "world" },
+                { 3, "!" }
+            }
+        );
+        PrintDictionary(new Dictionary<int, string>
+            {
+                { 54, "apple" },
+                { 21, "!@#$%^&*()_+" },
+            }
+        );
+        PrintDictionary(new Dictionary<int, string>
+            {
+                { 4, "C#" },
+                { 16, "1234567890" },
+                { 887, "I love my dog" },
+                { 654, "Ctrl C + Ctrl V" }
+            }
+        );
         
-        // NumberOfWords
+        // NumberOfSymbol
         Console.WriteLine();
-        Console.WriteLine("NumberOfWords");
-        Console.WriteLine(NumberOfWords("Hello my dear friend!"));
-        Console.WriteLine(NumberOfWords("How are you?"));
-        Console.WriteLine(NumberOfWords("I love my country!"));
+        Console.WriteLine("NumberOfSymbol");
+        Console.WriteLine(NumberOfSymbol("abca"));
+        Console.WriteLine(NumberOfSymbol("Hello world"));
+        Console.WriteLine(NumberOfSymbol("OOooAAoAa"));
+
+        // CountOfElements
+        Console.WriteLine();
+        Console.WriteLine("CountOfElements");
     }
+    
 }
--- Tasks/Tasks6.cs
+++ Tasks/Tasks6.cs
@@ -2,14 +2,72 @@
 
 public static class Tasks6
 {
-    // TODO: Создать и реализовать метод, принимающий на вход словарь и выводящий его в консоль в формате
-    // { ключ = значение; ключ = значение; ключ = значение }
-    
-    // TODO: Создать и реализовать метод, принимающий на вход строку и считающий с помощью словаря количество каждого
-    // символа в этой строке. Затем необходимо найти символ, встречающийся наибольшее количество раз и вернуть
-    // KeyValuePair с этим символом и его количеством
-    
+    public static void PrintDictionary(Dictionary<int, string> dict)
+    {
+        var count = 1;
+        var result = "{ ";
+
+        foreach (var pair in dict)
+        {
+            result += $"{pair.Key} = {pair.Value}";
+
+            if (count < dict.Count)
+            {
+                result += "; ";
+            }
+
+            count++;
+        }
+
+        result += " }";
+
+        Console.WriteLine(result);
+    }
+
+    public static KeyValuePair<char, int> NumberOfSymbol(string str)
+    {
+        var lowStr = str.ToLower();
+        var dict = new Dictionary<char, int>();
+
+        foreach (var el in lowStr)
+        {
+            if (!dict.ContainsKey(el))
+            {
+                dict.Add(el, 1);
+            }
+            else
+            {
+                dict[el]++;
+            }
+        }
+
+        foreach (var pair in dict)
+        {
+            if (pair.Value == dict.Values.Max())
+            {
+                return pair;
+            }
+        }
+
+        throw new Exception();
+    }
+
     // TODO: Создать и реализовать метод, принимающий на вход список элементов любого типа и возвращающий количество
     // элементов этого списка, не считая повторяющиеся
     // {1, 2, 3, 4, 4, 3, 2, 0} => 5
+
+    public static int CountOfElements<T>(List<T> list)
+    {
+        var newList = new List<T>();
+
+        foreach (var el in list)
+        {
+            if (!newList.Contains(el))
+            {
+                newList.Add(el);
+            }
+        }
+
+        return newList.Count;
+    }
 }
